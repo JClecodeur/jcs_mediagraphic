77106ce9e3a892578bcbdc6ca82d0366c946c614
JClecodeur <j.chabanol@mediagraphic-groupe.fr>
2023-04-05 12:20:11 +0200

    Create setup.py

---
+++ jcs_mediagraphic/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='jcs_mediagraphic',
+    packages=find_packages(include=['jcs_mediagraphic']),
+    version='0.1.0',
+    description='Python library for MediaGraphic Group usages',
+    author='Jean',
+    license='MIT',
+    install_requires=[],
+)
