66e94f787c500faedcf57204a26549e4e8e791dc
JClecodeur <j.chabanol@mediagraphic-groupe.fr>
2023-04-05 12:41:06 +0200

    Test3

--- setup.py
+++ setup.py
@@ -2,12 +2,16 @@ from setuptools import find_packages, setup
 
 import jcs_mediagraphic
 
+def long_description():
+    with open('README.md', encoding='utf-8') as f:
+        return f.read()
+
 setup(
     name='jcs_mediagraphic',
-    packages=find_packages(include=['jcs_mediagraphic']),
-    version=jcs_mediagraphic.version,
+    version=jcs_mediagraphic.__version__,
     description='Python library for MediaGraphic Group usages',
-    author=jcs_mediagraphic.author,
-    license=jcs_mediagraphic.license,
+    author=jcs_mediagraphic.__author__,
+    license=jcs_mediagraphic.__license__,
+    packages=find_packages(include=['jcs_mediagraphic']),
     install_requires=[],
 )
